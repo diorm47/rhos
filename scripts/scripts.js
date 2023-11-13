2a30755c1925e11c54102f3e90d142e2f179ed6d
diorm47 <dior47m@gmail.com>
2023-11-13 12:54:16 +0500

    main page res

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -2,3 +2,9 @@ const toggleNavList = () => {
   const nav_menu_list = document.querySelector(".nav_menu_list");
   nav_menu_list.classList.toggle("open");
 };
+
+const toggleMobmenu = () => {
+  const nav_mob_menu = document.querySelector(".nav_mob_menu");
+  nav_mob_menu.classList.toggle("visible_menu");
+};
+
